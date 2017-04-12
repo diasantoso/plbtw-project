18e812a0dd7cdc2060740bd720d8925690adad64
Erikku_Seiichiro <ericz.kuruhyun27@gmail.com>
2017-04-12 13:47:12 +0700

    testes

---
+++ AndroidApp/testes/app/src/main/java/testes/testes/MainActivity.java
@@ -0,0 +1,13 @@
+package testes.testes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

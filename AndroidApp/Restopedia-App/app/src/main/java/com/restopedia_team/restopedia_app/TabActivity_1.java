532b7b6e1858cbde57b42b7baa6c2815d27379b2
heryatmo <heryatmobenediktus@gmail.com>
2017-05-01 15:58:12 +0700

    Login SignUp and TabHost V0.1

---
+++ AndroidApp/Restopedia-App/app/src/main/java/com/restopedia_team/restopedia_app/TabActivity_1.java
@@ -0,0 +1,15 @@
+package com.restopedia_team.restopedia_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HERYATMO on 01/05/2017.
+ */
+
+public class TabActivity_1 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_1);
+    }
+}

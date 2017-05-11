794b5012b8cdb028ca5d68b149f72bcad33e5436
Vincent Andri K <Vincent Andri K>
2017-05-11 08:36:23 +0700

    Get photo from gallery and camera function added

--- AndroidApp/Restopedia/app/src/main/java/com/restopedia_team/restopedia/TabActivity2.java
+++ AndroidApp/Restopedia/app/src/main/java/com/restopedia_team/restopedia/TabActivity2.java
@@ -1,13 +1,256 @@
 package com.restopedia_team.restopedia;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TabActivity2 extends Activity {
 
+    Bitmap bi = null;
+    boolean isColored;
+
+    boolean flag;
+
+    private int SIZE = 256;
+    // Red, Green, Blue
+    private int NUMBER_OF_COLOURS = 3;
+
+    public final int RED = 0;
+    public final int GREEN = 1;
+    public final int BLUE = 2;
+
+    ImageView img;
+
+    private int[][] colourBins;
+    private volatile boolean loaded = false;
+
+    float offset = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab2);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        if(metrics.densityDpi== DisplayMetrics.DENSITY_LOW)
+            offset = 0.75f;
+        else if(metrics.densityDpi== DisplayMetrics.DENSITY_MEDIUM)
+            offset = 1f;
+        else if(metrics.densityDpi== DisplayMetrics.DENSITY_TV)
+            offset = 1.33f;
+        else if(metrics.densityDpi== DisplayMetrics.DENSITY_HIGH)
+            offset = 1.5f;
+        else if(metrics.densityDpi== DisplayMetrics.DENSITY_XHIGH)
+            offset = 2f;
+
+        Log.e("NIRAV",""+offset);
+
+        colourBins = new int[NUMBER_OF_COLOURS][];
+        for (int i = 0; i < NUMBER_OF_COLOURS; i++) {
+            colourBins[i] = new int[SIZE];
+        }
+
+        img = (ImageView) findViewById(R.id.original_pic);
+
+        loaded = false;
+
+        /// UNTUK AMBIL GAMBAR DARI GALLERY
+        Button gallery = (Button) findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent it = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(it, 101);
+                flag = true;
+
+            }
+        });
+
+        /// UNTUK AMBIL GAMBAR DARI KAMERA
+
+        Button camera = (Button) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(cameraIntent, 102);
+                flag=true;
+            }
+        });
+
+    }
+
+    private File getFile()
+    {
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+        return new File(folder,"cam_image.jpg");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+
+            case 101:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    String filename = getRealPathFromURI(selectedImage);
+                    bi = BitmapFactory.decodeFile(filename);
+                /*
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                bi.compress(Bitmap.CompressFormat.JPEG,10,out);
+                bi = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));*/
+
+                    if (bi != null) {
+                        try {
+                            new MyAsync().execute();
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            case 102:
+                String path="sdcard/camera_app/cam_image.jpg";
+                img.setImageDrawable(Drawable.createFromPath(path));
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        Log.e("TEST", "GetRealPath : " + contentUri);
+
+        try {
+            if (contentUri.toString().contains("video")) {
+                String[] proj = { MediaStore.Video.Media.DATA };
+                Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+                int column_index = cursor
+                        .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            } else {
+                String[] proj = { MediaStore.Images.Media.DATA };
+                Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+                int column_index = cursor
+                        .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            }
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    class MyAsync extends AsyncTask
+    {
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            showDialog(0);
+        }
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            // TODO Auto-generated method stub
+
+            try {
+                load(bi);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+
+            img.setImageBitmap(bi);
+            dismissDialog(0);
+            isColored = true;
+        }
+
+    }
+
+    public void load(Bitmap bi) throws IOException {
+
+        if (bi != null) {
+            // Reset all the bins
+            for (int i = 0; i < NUMBER_OF_COLOURS; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    colourBins[i][j] = 0;
+                }
+            }
+
+            for (int x = 0; x < bi.getWidth(); x++) {
+                for (int y = 0; y < bi.getHeight(); y++) {
+
+                    int pixel = bi.getPixel(x, y);
+
+                    colourBins[RED][Color.red(pixel)]++;
+                    colourBins[GREEN][Color.green(pixel)]++;
+                    colourBins[BLUE][Color.blue(pixel)]++;
+                }
+            }
+
+            int maxY = 0;
+
+            for (int i = 0; i < NUMBER_OF_COLOURS; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (maxY < colourBins[i][j]) {
+                        maxY = colourBins[i][j];
+                    }
+                }
+            }
+            loaded = true;
+        } else {
+            loaded = false;
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        ProgressDialog dataLoadProgress = new ProgressDialog(this);
+        dataLoadProgress.setMessage("Loading...");
+        dataLoadProgress.setIndeterminate(true);
+        dataLoadProgress.setCancelable(false);
+        dataLoadProgress.setProgressStyle(android.R.attr.progressBarStyleLarge);
+        return dataLoadProgress;
     }
 }
